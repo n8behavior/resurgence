e27c25459086dc514cece7dd2e0a3bd8cc561827
Mike Sandman <mike@thesandmans.com>
2025-07-06 01:12:21 -0400

    SimpleMulti noise with vertex coloring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use bevy::{
+    color::palettes::tailwind::{AMBER_800, GREEN_400},
     pbr::wireframe::{Wireframe, WireframeConfig, WireframePlugin},
     prelude::*,
     render::{
         RenderPlugin,
+        mesh::VertexAttributeValues,
         render_resource::WgpuFeatures,
         settings::{RenderCreation, WgpuSettings},
     },
 };
 use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+use noise::{BasicMulti, NoiseFn, Perlin};
 
 fn main() {
     App::new()
@@ -31,6 +34,7 @@ fn main() {
         })
         // 4) Your startup systems
         .add_systems(Startup, (setup_camera, setup_terrain, setup_light))
+        .add_systems(Update, toggle_wireframe)
         .run();
 }
 
@@ -68,7 +72,48 @@ fn setup_terrain(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let mesh = meshes.add(Plane3d::default().mesh().size(50.0, 50.0).subdivisions(10));
+    let terrain_height = 70f32;
+    let noise = BasicMulti::<Perlin>::default();
+
+    let mut terrain = Mesh::from(
+        Plane3d::default()
+            .mesh()
+            .size(1000f32, 1000f32)
+            .subdivisions(200),
+    );
+    if let Some(VertexAttributeValues::Float32x3(positions)) =
+        terrain.attribute_mut(Mesh::ATTRIBUTE_POSITION)
+    {
+        for pos in positions.iter_mut() {
+            let val = noise.get([(pos[0] / 300f32) as f64, (pos[2] / 300f32) as f64]);
+            pos[1] = val as f32 * terrain_height; // safe: Perlin is -1 to 1
+        }
+        let colors: Vec<[f32; 4]> = positions
+            .iter()
+            .map(|[_, g, _]| {
+                let g = *g / terrain_height * 2f32;
+                if g > 0.8f32 {
+                    (Color::LinearRgba(LinearRgba {
+                        red: 20f32,   // bloom above 1
+                        green: 20f32, // bloom above 1
+                        blue: 20f32,  // bloom above 1
+                        alpha: 1f32,
+                    }))
+                    .to_linear()
+                    .to_f32_array()
+                } else if g > 0.3f32 {
+                    Color::from(AMBER_800).to_linear().to_f32_array()
+                } else if g < -0.8f32 {
+                    Color::BLACK.to_linear().to_f32_array()
+                } else {
+                    Color::from(GREEN_400).to_linear().to_f32_array()
+                }
+            })
+            .collect();
+        terrain.insert_attribute(Mesh::ATTRIBUTE_COLOR, colors);
+    }
+    terrain.compute_normals();
+    let mesh = meshes.add(terrain);
     let material = materials.add(StandardMaterial {
         base_color: bevy::color::palettes::css::SILVER.into(),
         ..default()
@@ -81,6 +126,24 @@ fn setup_terrain(
         Transform::from_xyz(0.0, 0.0, 0.0),
         GlobalTransform::default(),
         // 5) turn on wireframe for this mesh
-        Wireframe,
+        Terrain,
     ));
+}
+#[derive(Component)]
+struct Terrain;
+
+fn toggle_wireframe(
+    mut commands: Commands,
+    landscapes_wireframe: Query<Entity, (With<Terrain>, With<Wireframe>)>,
+    landscapes: Query<Entity, (With<Terrain>, Without<Wireframe>)>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        for terrain in &landscapes {
+            commands.entity(terrain).insert(Wireframe);
+        }
+        for terrain in &landscapes_wireframe {
+            commands.entity(terrain).remove::<Wireframe>();
+        }
+    }
 }
