1768d189d87ae6dbd1e7b8b258199f48404fca25
Mike Sandman <mike@thesandmans.com>
2025-07-14 13:16:08 -0400

    Add the `bevy_ecs_tilemap` crate
    - Introduce `bevy_ecs_tilemap` dependency in `Cargo.toml` with specified features.
    - Update `Cargo.lock` to include version `0.16.0` of `bevy_ecs_tilemap`.
    - Add a new texture asset `textures_16x16.png` to be used in the tilemap.
    - Implement tilemap initialization in the `ecs_tilemap.rs` experiment module, spawning a tilemap and configuring its settings such as grid size, map size, and texture.

--- src/experiments/ecs_tilemap.rs
+++ src/experiments/ecs_tilemap.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 
 use super::{AppState, Experiment};
 
@@ -18,6 +19,44 @@ impl Experiment for EcsTilemapPoc {
     }
 
     fn app_setup<'a>(&self, app: &'a mut App) -> &'a mut App {
-        app
+        app.add_plugins(TilemapPlugin)
+            .add_systems(OnEnter(AppState::EcsTilemap), startup)
     }
+}
+
+fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d);
+
+    let map_size = TilemapSize { x: 32, y: 32 };
+    let mut tile_storage = TileStorage::empty(map_size);
+    let tilemap_entity = commands.spawn_empty().id();
+    for x in 0..map_size.x {
+        for y in 0..map_size.y {
+            let tile_pos = TilePos { x, y };
+            let tile_entity = commands
+                .spawn(TileBundle {
+                    position: tile_pos,
+                    tilemap_id: TilemapId(tilemap_entity),
+                    ..default()
+                })
+                .id();
+            tile_storage.set(&tile_pos, tile_entity);
+        }
+    }
+
+    let texture_handle: Handle<Image> = asset_server.load("textures_16x16.png");
+    let tile_size = TilemapTileSize { x: 16f32, y: 16f32 };
+    let grid_size = tile_size.into();
+    let map_type = TilemapType::Square;
+
+    commands.entity(tilemap_entity).insert(TilemapBundle {
+        grid_size,
+        map_type,
+        size: map_size,
+        storage: tile_storage,
+        texture: TilemapTexture::Single(texture_handle),
+        tile_size,
+        anchor: TilemapAnchor::Center,
+        ..default()
+    });
 }
